307b470ccabf5b0b3ecfc6e2e1dd7461893271c1
michaeldecent2 <111002205+MichaelDecent@users.noreply.github.com>
2024-02-12 19:11:03 +0100

    added README.md

---
+++ utils/db.js
@@ -0,0 +1,29 @@
+const { MongoClient } = require('mongodb');
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || 27017;
+    this.database = process.env.DB_DATABASE || 'files_manager';
+
+    const url = `mongodb://${host}:${port}`;
+
+    this.client = new MongoClient(url);
+    this.client.connect()
+      .then(() => {
+        this.connected = true;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  isAlive() {
+    return this.connected;
+  }
+
+  async nbUsers() {
+    
+  }
+
+}
